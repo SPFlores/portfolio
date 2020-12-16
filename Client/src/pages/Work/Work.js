9455114b2e26584ce37f821e2e64e10d74b4895d
SPFlores <sflores92886@gmail.com>
2020-12-15 16:43:27 -0800

    laying out work page state

--- Client/src/pages/Work/Work.js
+++ Client/src/pages/Work/Work.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const WorkPage = _ => {
+  const [workState, setWorkState] = useState({
+    something: false
+  })
+
+  // useEffect(_ => {
+  //   setWorkState({ ...workState, something: !workState.something })
+  // }, [])
+
+  const doSomething = _ => {
+    console.log(workState.something)
+  }
+
+  const doSomethingElse = _ => {
+    setWorkState({ ...workState, something: !workState.something })
+  }
+
   return (
-    <div>This is the *Work* page!</div>
+    <div>
+      <div>
+        This is the *Work* page!
+      </div>
+      <button onClick={doSomething}>Click on me please</button>
+      <br />
+      <button onClick={doSomethingElse}>No click on me</button>
+    </div>
   )
 }
 
