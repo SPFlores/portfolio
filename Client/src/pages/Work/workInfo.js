11297051c06c76fe35b91aa660b5750916ec700f
SPFlores <sflores92886@gmail.com>
2020-12-16 12:28:04 -0800

    layout for project cards

--- Client/src/pages/Work/workInfo.js
+++ Client/src/pages/Work/workInfo.js
@@ -6,6 +6,62 @@ const projects = [
     repoLink: 'repo.link',
     skills: ['JS', 'React'],
     picture: 'picturelink.jpeg'
+  },
+  {
+    title: 'a thing',
+    description: 'stuff',
+    deployedLink: 'deployed.link',
+    repoLink: 'repo.link',
+    skills: ['JS', 'React'],
+    picture: 'picturelink.jpeg'
+  },
+  {
+    title: 'a thing',
+    description: 'stuff',
+    deployedLink: 'deployed.link',
+    repoLink: 'repo.link',
+    skills: ['JS', 'React'],
+    picture: 'picturelink.jpeg'
+  },
+  {
+    title: 'a thing',
+    description: 'stuff',
+    deployedLink: 'deployed.link',
+    repoLink: 'repo.link',
+    skills: ['JS', 'React'],
+    picture: 'picturelink.jpeg'
+  },
+  {
+    title: 'a thing',
+    description: 'stuff',
+    deployedLink: 'deployed.link',
+    repoLink: 'repo.link',
+    skills: ['JS', 'React'],
+    picture: 'picturelink.jpeg'
+  },
+  {
+    title: 'a thing',
+    description: 'stuff',
+    deployedLink: 'deployed.link',
+    repoLink: 'repo.link',
+    skills: ['JS', 'React'],
+    picture: 'picturelink.jpeg'
+  },
+  {
+    title: 'a thing',
+    description: 'stuff',
+    deployedLink: 'deployed.link',
+    repoLink: 'repo.link',
+    skills: ['JS', 'React'],
+    picture: 'picturelink.jpeg'
+  },
+  {
+    title: 'a thing',
+    description: 'stuff',
+    deployedLink: 'deployed.link',
+    repoLink: 'repo.link',
+    skills: ['JS', 'React'],
+    picture: 'picturelink.jpeg'
   }
 ]
 
