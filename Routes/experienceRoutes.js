2159c37639d1a60c1a350dbe5d9672317e252a47
SPFlores <sflores92886@gmail.com>
2020-12-15 16:24:52 -0800

    experience routes

---
+++ Routes/experienceRoutes.js
@@ -0,0 +1,38 @@
+const { Experience } = require('../models')
+
+module.exports = app => {
+  app.get('/experiences', (req, res) => {
+    Experience.findAll()
+      .then(experiences => res.json(experiences))
+      .catch(e => console.log(e))
+  })
+
+  app.get('/experiences/:id', (req, res) => {
+    Experience.findOne({ where: { id: req.params.id } })
+      .then(experience => res.json(experience))
+      .catch(e => console.log(e))
+  })
+
+  // must be logged in
+  app.post('/experiences', (req, res) => {
+    Experience.create(req.body)
+      .then(_ => {
+        res.sendStatus(200)
+      })
+      .catch(e => console.log(e))
+  })
+
+  // must be logged in
+  app.put('/experiences/:id', (req, res) => {
+    Experience.update(req.body, { where: { id: req.params.id } })
+      .then(_ => res.sendStatus(200))
+      .catch(e => console.log(e))
+  })
+
+  // must be logged in
+  app.delete('./experiences/:id', (req, res) => {
+    Experience.destroy({ where: { id: req.params.id } })
+      .then(_ => res.sendStatus(200))
+      .catch(e => console.log(e))
+  })
+}
