bccbe0413746864c498f1ef4656c69003d57f0e0
SPFlores <sflores92886@gmail.com>
2020-12-16 12:00:58 -0800

    setting up greeting section

--- Client/src/pages/Greeting/Greeting.js
+++ Client/src/pages/Greeting/Greeting.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import './index.css'
 
 const Welcome = _ => {
   return (
-    <div className='sectionHeading' id='gretting'>
-      <h2>Hello, I'm Sammi!</h2>
-      <h1>Full stack developer, front end developer, avid learner, curious nerd.</h1>
-      <p>Info about me goes here, professional statement?</p>
-      <button>Let's chat!</button>
+    <div className='sectionHeading' id='greeting'>
+      <h2 id='greeting__name'>Hello, I'm Sammi!</h2>
+      <h4 id='greeting__description'>I am a full stack developer specializing in front end development, an avid learner, and a curious nerd.</h4>
+      <p id='greeting__info'>I love to play around in Javascript, especially React, and enjoy making powerful webpages with clean, reusable code. I have professional experience in a fast-paced start up environment and enjoy tackling challenges with a collaborative team of developers by my side.</p>
+      <a id='greeting__button' href='mailto:sflores92886@gmail.com'>Let's talk!</a>
     </div>
   )
 }
