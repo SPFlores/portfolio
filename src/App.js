e4e31076d60bdeee37bcf5f660f051e2a40bf5bd
SPFlores <sflores92886@gmail.com>
2020-12-16 23:25:48 -0800

    deploying to pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Landing from './pages/Landing'
-import Resume from './pages/Resume'
 import './App.css'
 
 const App = _ => {
   return (
-    <Router>
-      <Route exact path='/' render={_ => (<Landing />)} />
-      <Route path='/resume' render={_ => (<Resume />)} />
-    </Router>
+    <Landing />
   )
 }
 
